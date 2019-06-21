0613d9e80103a69d6edb9dff6e4e7a2eea28934b
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-21 11:34:22 -0500

    Implement toasts service

--- projects/kurtz1993/ngx-toasts/src/lib/services/ngx-toasts.service.ts
+++ projects/kurtz1993/ngx-toasts/src/lib/services/ngx-toasts.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject, queueScheduler } from 'rxjs';
+import { observeOn } from 'rxjs/operators';
+
+import { NgxToastConfig } from '../models/toast-config.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NgxToastsService {
+  toasts$: Observable<NgxToastConfig>;
 
-  constructor() { }
+  private defaultConfig: NgxToastConfig = {
+    message: '',
+    type: 'success',
+    canBeClosed: true,
+    timeout: 5000,
+  };
+  private toastsHandler = new Subject<NgxToastConfig>();
+
+  constructor() {
+    this.toasts$ = this.toastsHandler.pipe(observeOn(queueScheduler));
+  }
+
+  /**
+   * Shows a toast on the screen with the given configuration.
+   * @param config Configuration needed to show the toast.
+   */
+  showToast(config: NgxToastConfig): void {
+    const toastConfig = { ...this.defaultConfig, ...config };
+    this.toastsHandler.next(toastConfig);
+  }
 }
