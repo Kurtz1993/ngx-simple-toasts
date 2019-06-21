f30a4d2914eb90a2c223103505fa935e25f99ca2
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-21 16:17:47 -0500

    Add first unit test

--- projects/kurtz1993/ngx-toasts/src/lib/services/ngx-toasts.service.spec.ts
+++ projects/kurtz1993/ngx-toasts/src/lib/services/ngx-toasts.service.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NgxToastsService } from './ngx-toasts.service';
+import { NgxToastConfig } from '../models/toast-config.model';
 
 describe('NgxToastsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: NgxToastsService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(NgxToastsService);
+  });
 
   it('should be created', () => {
-    const service: NgxToastsService = TestBed.get(NgxToastsService);
     expect(service).toBeTruthy();
+  });
+
+  it('should emit a new toast when calling showToast()', () => {
+    const config: NgxToastConfig = {
+      message: 'hello',
+      type: 'success',
+    };
+
+    const subjectSpy = spyOn(service['toastsHandler'], 'next');
+
+    service.showToast(config);
+
+    expect(subjectSpy).toHaveBeenCalled();
   });
 });
