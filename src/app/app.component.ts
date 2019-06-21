8a8b3e388f52341be2f6fe021bf2aa282167b26d
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-21 11:58:08 -0500

    Add mehtod to test toasts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgxToastsService } from 'projects/kurtz1993/ngx-toasts/src/public-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'toasts';
+
+  constructor(private toastService: NgxToastsService) {}
+
+  createToast(toastType: 'success' | 'error' | 'warning' | 'info'): void {
+    this.toastService.showToast({ type: toastType, message: 'Hello' });
+  }
 }
