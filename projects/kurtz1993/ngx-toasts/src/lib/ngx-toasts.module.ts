a5f6a73b5afdc54ee48e57b4953e989decf28a1b
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-21 11:40:37 -0500

    Register and export ngx toasts container

--- projects/kurtz1993/ngx-toasts/src/lib/ngx-toasts.module.ts
+++ projects/kurtz1993/ngx-toasts/src/lib/ngx-toasts.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NgxToastComponent } from './components/ngx-toast/ngx-toast.component';
+import { NgxToastsContainerComponent } from './components/ngx-toasts-container/ngx-toasts-container.component';
 import { NgxToastsService } from './services/ngx-toasts.service';
 
 @NgModule({
-  declarations: [NgxToastComponent],
+  declarations: [NgxToastComponent, NgxToastsContainerComponent],
   providers: [NgxToastsService],
-  imports: [
-    CommonModule
-  ],
-  exports: [NgxToastComponent]
+  imports: [CommonModule],
+  exports: [NgxToastComponent, NgxToastsContainerComponent],
 })
-export class NgxToastsModule { }
+export class NgxToastsModule {}
