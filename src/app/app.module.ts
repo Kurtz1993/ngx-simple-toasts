f1ca2aef7b1d766463206f15fc99c88a3ef97584
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-21 11:57:58 -0500

    Add ngx toast module for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxToastsModule } from 'projects/kurtz1993/ngx-toasts/src/public-api';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxToastsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
