127622dfc4fb4bf9bc86b5ab16373e174453e78b
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-21 11:23:18 -0500

    Adjust public-api

--- projects/kurtz1993/ngx-toasts/src/public-api.ts
+++ projects/kurtz1993/ngx-toasts/src/public-api.ts
@@ -3,5 +3,5 @@
  */
 
 export * from './lib/services/ngx-toasts.service';
-export * from './lib/ngx-toasts.component';
+export * from './lib/components/ngx-toast/ngx-toast.component';
 export * from './lib/ngx-toasts.module';
