3eb42c87d205e67e8780a794c4f1713fc93a7719
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-21 11:34:49 -0500

    Register NgxToastsService

--- projects/kurtz1993/ngx-toasts/src/lib/ngx-toasts.module.ts
+++ projects/kurtz1993/ngx-toasts/src/lib/ngx-toasts.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NgxToastComponent } from './components/ngx-toast/ngx-toast.component';
+import { NgxToastsService } from './services/ngx-toasts.service';
 
 @NgModule({
   declarations: [NgxToastComponent],
+  providers: [NgxToastsService],
   imports: [
     CommonModule
   ],
