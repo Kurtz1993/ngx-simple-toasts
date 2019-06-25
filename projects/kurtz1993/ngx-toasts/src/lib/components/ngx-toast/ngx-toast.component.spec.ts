b0cd21b58fc333adaf869c5aa739847739ded019
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-25 12:57:05 -0500

    Start adding unit tests for the toast component

--- projects/kurtz1993/ngx-toasts/src/lib/components/ngx-toast/ngx-toast.component.spec.ts
+++ projects/kurtz1993/ngx-toasts/src/lib/components/ngx-toast/ngx-toast.component.spec.ts
@@ -1,25 +1,100 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { NgxToastComponent } from './ngx-toast.component';
+import { NgxToastConfig } from '../../models/toast-config.model';
 
 describe('NgxToastsComponent', () => {
+  let config: NgxToastConfig;
   let component: NgxToastComponent;
   let fixture: ComponentFixture<NgxToastComponent>;
+  let elm: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NgxToastComponent ]
-    })
-    .compileComponents();
+      declarations: [NgxToastComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NgxToastComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    elm = fixture.debugElement;
+    config = {
+      message: '',
+      timeout: 1000,
+      type: 'success',
+    };
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should close itself after the specified timeout', fakeAsync(() => {
+    const closeSpy = spyOn(component, 'closeToast');
+
+    component.config = config;
+    fixture.detectChanges();
+
+    tick(config.timeout);
+
+    expect(closeSpy).toHaveBeenCalled();
+  }));
+
+  it('should show a progressbar with the correct animation duration if timeout is specified', () => {
+    const durationInMs = `${config.timeout}ms`;
+    component.config = config;
+
+    fixture.detectChanges();
+    const progressbar = elm.query(By.css('.progressbar'));
+
+    expect(component.animationDuration).toBe(durationInMs);
+    expect(progressbar).toBeTruthy();
+    expect(progressbar.styles.animationDuration).toBe(durationInMs);
+  });
+
+  it('should not show a progressbar if timeout is set to 0', () => {
+    config.timeout = 0;
+    component.config = config;
+
+    fixture.detectChanges();
+    const progressbar = elm.query(By.css('.progressbar'));
+
+    expect(progressbar).toBeNull();
+  });
+
+  it('should correctly set the modifier class based on the config type', () => {
+    component.config = config;
+
+    fixture.detectChanges();
+
+    expect(component.componentClasses).toContain(`ngx-toast--${config.type}`);
+  });
+
+  it('should correctly place the message in the toast', () => {
+    config.message = 'This is a test message';
+    component.config = config;
+    const msgParragraph = elm.query(By.css('.ngx-toast__message'));
+
+    fixture.detectChanges();
+
+    expect(msgParragraph.nativeElement.innerText).toContain(config.message);
+  });
+
+  it('should add a class for closing and wait 500ms and emit remove event when clicking on close button', fakeAsync(() => {
+    config.timeout = 0;
+    component.config = config;
+    const closeBtn = elm.query(By.css('.ngx-toast__close'));
+    const removeSpy = spyOn(component.remove, 'emit');
+
+    fixture.detectChanges();
+    closeBtn.triggerEventHandler('click', null);
+    expect(component.componentClasses).toContain('ngx-toast--closing');
+
+    tick(500);
+
+    expect(removeSpy).toHaveBeenCalledWith(component.config.id);
+  }));
 });
